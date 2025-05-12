c933a851bf826efd8d18d9d1aaceff9cf14277ca
Zeh Fernando <zeh@zehfernando.com>
2025-05-12 12:05:43 -0400

    Added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+fn parse_image_dimensions(s: &str) -> Result<(u32, u32), String> {
+    let parts: Vec<&str> = s.split('x').collect();
+    if parts.len() != 2 {
+        return Err("Invalid image dimensions; use WIDTHxHEIGHT".to_string());
+    }
+    let width = parts[0].parse::<u32>().map_err(|_| "Invalid width")?;
+    let height = parts[1].parse::<u32>().map_err(|_| "Invalid height")?;
+    Ok((width, height))
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "face-align-composite", about = "Composite face-aligned images.")]
+struct Opt {
+    /// File mask (e.g., "images/*.jpg")
+    #[structopt(long, default_value = "*.jpg")]
+    input: String,
+
+    /// Output image dimensions (e.g., "800x600")
+    #[structopt(long, default_value = "1024x1024", parse(try_from_str = parse_image_dimensions))]
+    dimensions: (u32, u32),
+
+    /// Output file name (e.g., "output.png")
+    #[structopt(long, default_value = "face-stack-output.jpg", parse(from_os_str))]
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    let (target_width, target_height) = opt.dimensions;
+
+    println!("Will get files from {:?}, at size {}x{}, and output at {:?}.", opt.input, target_width, target_height, opt.output);
 }
