8f44cddb84b11a9d330648e600fddbb0438c5854
Zeh Fernando <zeh@zehfernando.com>
2025-05-13 21:18:29 -0400

    Terminal: added one more method to move the cursor

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,12 @@
 // From https://github.com/zeh/random-art-generator/blob/main/src/generator/utils/terminal.rs
+// More info: https://en.wikipedia.org/wiki/ANSI_escape_code
 
 pub fn cursor_up() {
 	print!("\u{1b}[1;A");
+}
+
+pub fn cursor_start_of_line() {
+	print!("\u{1b}[1;G");
 }
 
 pub fn erase_line_to_end() {
