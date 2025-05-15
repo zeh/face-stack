9dd91e90553c08d02e03cd28ed26527e486dab35
Zeh Fernando <zeh@zehfernando.com>
2025-05-15 13:25:32 -0400

    Add new position/size type aliases to geom module

--- src/geom.rs
+++ src/geom.rs
@@ -1,7 +1,15 @@
+pub type XYf = (f32, f32);
+pub type WHf = (f32, f32);
+pub type XYWHf = (f32, f32, f32, f32);
+
+pub type XYi = (i32, i32);
+pub type WHi = (u32, u32);
+pub type XYWHi = (i32, i32, u32, u32);
+
 /**
  * Find the expected scale to fit a rectangle (w, h) inside another.
  */
-pub fn fit_inside(outside_rect: (f32, f32), inside_rect: (f32, f32)) -> (f32, f32) {
+pub fn fit_inside(outside_rect: WHf, inside_rect: WHf) -> WHf {
 	let inside_ar = inside_rect.0 / inside_rect.1;
 	let outside_ar = outside_rect.0 / outside_rect.1;
 	if inside_ar > outside_ar {
@@ -11,4 +19,12 @@ pub fn fit_inside(outside_rect: (f32, f32), inside_rect: (f32, f32)) -> (f32, f3
 		// Inside rect width is "taller" than outside: fit by its height
 		(outside_rect.1 * inside_ar, outside_rect.1)
 	}
+}
+
+pub fn xyf_to_xyi(xy: XYf) -> XYi {
+	(xy.0.round() as i32, xy.1.round() as i32)
+}
+
+pub fn whf_to_whi(wh: WHf) -> WHi {
+	(wh.0.round() as u32, wh.1.round() as u32)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rust_faces::{
 use structopt::StructOpt;
 
 use blending::{BlendingMode, blend_pixel, pixel_u8_to_f32};
-use geom::fit_inside;
+use geom::{WHf, WHi, XYi, fit_inside, whf_to_whi, xyf_to_xyi};
 
 pub mod blending;
 pub mod geom;
@@ -33,27 +33,27 @@ fn parse_image_dimensions(s: &str) -> Result<(u32, u32), String> {
 fn blend_image(
 	bottom: &mut Rgb32FImage,
 	top: &RgbImage,
-	offset: (i32, i32),
+	top_offset: XYi,
 	opacity: f32,
 	blending_mode: &BlendingMode,
 ) {
-	let src_x1 = if offset.0 < 0 {
-		-offset.0 as u32
+	let src_x1 = if top_offset.0 < 0 {
+		-top_offset.0 as u32
 	} else {
 		0
 	};
-	let src_x2 = top.width().min((bottom.width() as i32 - offset.0) as u32) - 1;
-	let src_y1 = if offset.1 < 0 {
-		-offset.1 as u32
+	let src_x2 = top.width().min((bottom.width() as i32 - top_offset.0) as u32) - 1;
+	let src_y1 = if top_offset.1 < 0 {
+		-top_offset.1 as u32
 	} else {
 		0
 	};
-	let src_y2 = top.height().min((bottom.height() as i32 - offset.1) as u32) - 1;
+	let src_y2 = top.height().min((bottom.height() as i32 - top_offset.1) as u32) - 1;
 
 	for src_y in src_y1..src_y2 {
-		let dst_y = (src_y as i32 + offset.1) as u32;
+		let dst_y = (src_y as i32 + top_offset.1) as u32;
 		for src_x in src_x1..src_x2 {
-			let dst_x = (src_x as i32 + offset.0) as u32;
+			let dst_x = (src_x as i32 + top_offset.0) as u32;
 			let bottom_px: [f32; 3] = bottom
 				.get_pixel(dst_x, dst_y)
 				.channels()
@@ -125,10 +125,10 @@ fn main() {
             .expect("Failed to load the face detector");
 
 	// Decide where the face will be in the output image
-	let typical_face_size = (75f32, 100f32); // Typically 0.75 aspect ratio
+	let typical_face_size: WHf = (75f32, 100f32); // Typically 0.75 aspect ratio
 	let faces_rect_inside = fit_inside((target_width as f32, target_height as f32), typical_face_size);
 	let typical_face_scale = 0.6f32 * opt.face_scale;
-	let target_faces_rect =
+	let target_faces_rect: WHf =
 		(faces_rect_inside.0 * typical_face_scale, faces_rect_inside.1 * typical_face_scale);
 
 	// Create the output image
@@ -180,36 +180,30 @@ fn main() {
 					let face_rect = &faces[0].rect;
 
 					// Find out what the face size should be inside our face target box
-					let target_face_rect = fit_inside(target_faces_rect, (face_rect.width, face_rect.height));
+					let target_face_rect: WHf =
+						fit_inside(target_faces_rect, (face_rect.width, face_rect.height));
 					let new_image_scale = target_face_rect.0 / face_rect.width;
-					let new_image_size = (
-						(rgb_image.width() as f32 * new_image_scale).round() as u32,
-						(rgb_image.height() as f32 * new_image_scale).round() as u32,
-					);
+					let new_image_size: WHi = whf_to_whi((
+						rgb_image.width() as f32 * new_image_scale,
+						rgb_image.height() as f32 * new_image_scale,
+					));
 
 					// Scale the image appropriately
 					let resized_image =
 						imageops::resize(&rgb_image, new_image_size.0, new_image_size.1, imageops::Lanczos3);
 
 					// Finally, blend it all
-					let offset = (
+					let offset: XYi = xyf_to_xyi((
 						target_width as f32 / 2.0 - (face_rect.x + face_rect.width / 2.0) * new_image_scale,
 						target_height as f32 / 2.0 - (face_rect.y + face_rect.height / 2.0) * new_image_scale,
-					);
-					let offset_i32 = (offset.0.round() as i32, offset.1.round() as i32);
+					));
 					if num_images_used == 0 {
-						blend_image(
-							&mut output_image,
-							&resized_image,
-							offset_i32,
-							1.0,
-							&BlendingMode::Normal,
-						);
+						blend_image(&mut output_image, &resized_image, offset, 1.0, &BlendingMode::Normal);
 					} else {
 						blend_image(
 							&mut output_image,
 							&resized_image,
-							offset_i32,
+							offset,
 							0.25, // 1f64 / (num_images_used as f64 + 1f64),
 							&all_blending_modes[num_images_used % all_blending_modes.len()],
 						);
